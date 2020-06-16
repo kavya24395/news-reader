12cb119471a7e60162231fb294fe4c4ebfa2a421
Kavya P S <kavyaps@Kavyas-MacBook-Pro.local>
2020-06-16 14:06:07 +0530

    Adding NewsViewModel

--- app/src/main/java/com/kavya/newsreader/MainActivity.kt
+++ app/src/main/java/com/kavya/newsreader/MainActivity.kt
@@ -2,11 +2,14 @@ package com.kavya.newsreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.kavya.newsreader.manager.NewsViewModel
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mNewsViewModel: NewsViewModel by viewModels<NewsViewModel>()
     }
 }
